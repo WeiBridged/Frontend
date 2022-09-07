c185b43fc251e3fa66caa64b2495915303b22509
Johanna Fransson <adrina0002@gmail.com>
2022-09-07 22:10:42 +0200

    added logic for swapping cross chain using debridge as swap provider API

--- src/pages/HomeWrap.js
+++ src/pages/HomeWrap.js
@@ -1,4 +1,50 @@
-const HomeWrap = () => {
+import React, { useState, useEffect } from "react";
+
+export default function HomeWrap({ interval, searchText }) {
+  const [estimateSwapData, setEstimateSwapData] = useState({});
+  const [getTransactionData, setGetTransactionData] = useState({});
+
+  let srcChainId = "1";
+  let srcChainTokenIn = "0xdAC17F958D2ee523a2206206994597C13D831ec7";
+  let srcChainTokenInAmount = "50000000";
+  let dstChainId = "137";
+  let dstChainTokenOut = "0x0000000000000000000000000000000000000000";
+  let dstChainTokenOutRecipient = "0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045";
+  let dstChainFallbackAddress = "0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045";
+
+  //IMPLEMENTAION FROM DEBRIDGE DOCS: https://docs.debridge.finance/deswap/api-quick-start-guide
+  /*   srcChainId specifies the Ethereum chain id (1) as the chain swap is being initiated
+srcChainTokenIn specifies the USDT token address (0xdAC17F958D2ee523a2206206994597C13D831ec7)
+srcChainTokenInAmount specifies the desired input amount: since USDT token contract uses 6 decimals (the number of digits that come after the decimal place when displaying token values on-screen), the simple math: 50 * 10^6 leads to 50000000 as the value representing 50 USDT tokens
+dstChainId specified the Polygon network chain id (137) as the target (destination) chain
+dstChainTokenOut specifies the address of the target token; since MATIC is not a typical ERC-20 token represented by a smart contract but rather a native coin (a one-of-a-kind token within each EVM chain), we use a null (or zero) address to distinguish it from other tokens.
+dstChainTokenOutRecipient, the address target tokens should be transferred to after the swap, and
+dstChainFallbackAddress, the address target or intermediary tokens should be transferred in case of a failed swap (e.g., a swap may fail due to slippage constraints). 
+
+*/
+
+  useEffect(() => {
+    const fetchSwapEstimation = async () => {
+      const liveUrl = `https://deswap.debridge.finance/v1.0/estimation?srcChainId=${srcChainId}&srcChainTokenIn=${srcChainTokenIn}&srcChainTokenInAmount=${srcChainTokenInAmount}&slippage=1&dstChainId=${dstChainId}&dstChainTokenOut=${dstChainTokenOut}&executionFeeAmount=auto`;
+      const res = await fetch(liveUrl);
+      const data = await res.json();
+      setEstimateSwapData(data);
+      await getTransaction();
+    };
+    fetchSwapEstimation();
+  }, []);
+
+  const getTransaction = async () => {
+    fetch(
+      `https://deswap.debridge.finance/v1.0/transaction?srcChainId=${srcChainId}&srcChainTokenIn=${srcChainTokenIn}&srcChainTokenInAmount=${srcChainTokenInAmount}&slippage=1&dstChainId=${dstChainId}&dstChainTokenOut=${dstChainTokenOut}&executionFeeAmount=auto&dstChainTokenOutRecipient=${dstChainTokenOutRecipient}&dstChainFallbackAddress=${dstChainFallbackAddress}`
+    )
+      .then((response) => response.json())
+      .then((response) => setGetTransactionData(response))
+      .catch((err) => console.error(err));
+  };
+
+  console.log(estimateSwapData, "est swap data");
+  console.log(getTransactionData, "gettransaction data");
   return (
     <body>
       <div id="root">
@@ -55,8 +101,6 @@ const HomeWrap = () => {
       </div>
     </body>
   );
-};
+}
 {
 }
-
-export default HomeWrap;
