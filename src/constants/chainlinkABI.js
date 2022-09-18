e8208e3c56752144ce23971e3786511900aba93d
Johanna Fransson <adrina0002@gmail.com>
2022-09-18 21:29:51 +0200

    add new abi and contract addr

--- src/constants/chainlinkABI.js
+++ src/constants/chainlinkABI.js
@@ -1,77 +1,14 @@
 export const goerliABI = [
-  {
-    inputs: [],
-    stateMutability: "nonpayable",
-    type: "constructor",
-  },
-  {
-    inputs: [],
-    name: "bridgeEmpty",
-    type: "error",
-  },
-  {
-    inputs: [],
-    name: "bridgeOnOtherSideNeedsLiqudity",
-    type: "error",
-  },
-  {
-    inputs: [],
-    name: "bridgedAlready",
-    type: "error",
-  },
-  {
-    inputs: [],
-    name: "msgValueDoesNotCoverFee",
-    type: "error",
-  },
-  {
-    inputs: [],
-    name: "msgValueLessThan1000",
-    type: "error",
-  },
-  {
-    inputs: [],
-    name: "msgValueZero",
-    type: "error",
-  },
-  {
-    inputs: [],
-    name: "notExternalBridge",
-    type: "error",
-  },
-  {
-    inputs: [],
-    name: "notOwnerAddress",
-    type: "error",
-  },
-  {
-    inputs: [],
-    name: "ownerBridgeUsersBeforeWithdraw",
-    type: "error",
-  },
-  {
-    inputs: [],
-    name: "queueIsEmpty",
-    type: "error",
-  },
-  {
-    inputs: [],
-    name: "queueNotEmpty",
-    type: "error",
-  },
-  {
-    inputs: [],
-    name: "Owner",
-    outputs: [
-      {
-        internalType: "address",
-        name: "",
-        type: "address",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
+  { inputs: [], name: "bridgeEmpty", type: "error" },
+  { inputs: [], name: "bridgeOnOtherSideNeedsLiqudity", type: "error" },
+  { inputs: [], name: "bridgedAlready", type: "error" },
+  { inputs: [], name: "msgValueNot1003", type: "error" },
+  { inputs: [], name: "msgValueZero", type: "error" },
+  { inputs: [], name: "notExternalBridge", type: "error" },
+  { inputs: [], name: "notOwnerAddress", type: "error" },
+  { inputs: [], name: "ownerBridgeUsersBeforeWithdraw", type: "error" },
+  { inputs: [], name: "queueIsEmpty", type: "error" },
+  { inputs: [], name: "queueNotEmpty", type: "error" },
   {
     inputs: [],
     name: "automatedUnlockGoerliETH",
@@ -80,25 +17,11 @@ export const goerliABI = [
     type: "function",
   },
   {
-    inputs: [
-      {
-        internalType: "bytes",
-        name: "",
-        type: "bytes",
-      },
-    ],
+    inputs: [{ internalType: "bytes", name: "", type: "bytes" }],
     name: "checkUpkeep",
     outputs: [
-      {
-        internalType: "bool",
-        name: "upkeepNeeded",
-        type: "bool",
-      },
-      {
-        internalType: "bytes",
-        name: "",
-        type: "bytes",
-      },
+      { internalType: "bool", name: "upkeepNeeded", type: "bool" },
+      { internalType: "bytes", name: "", type: "bytes" },
     ],
     stateMutability: "nonpayable",
     type: "function",
@@ -112,105 +35,16 @@ export const goerliABI = [
   },
   {
     inputs: [],
-    name: "first",
-    outputs: [
-      {
-        internalType: "uint256",
-        name: "",
-        type: "uint256",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [
-      {
-        internalType: "address",
-        name: "",
-        type: "address",
-      },
-    ],
-    name: "goerliBridgedETH",
-    outputs: [
-      {
-        internalType: "uint256",
-        name: "",
-        type: "uint256",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [],
-    name: "last",
-    outputs: [
-      {
-        internalType: "uint256",
-        name: "",
-        type: "uint256",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [
-      {
-        internalType: "uint256",
-        name: "bridgeAmount",
-        type: "uint256",
-      },
-    ],
     name: "lockTokensForOptimism",
     outputs: [],
     stateMutability: "payable",
     type: "function",
   },
   {
-    inputs: [
-      {
-        internalType: "address",
-        name: "",
-        type: "address",
-      },
-    ],
-    name: "lockedForOptimismETH",
-    outputs: [
-      {
-        internalType: "uint256",
-        name: "",
-        type: "uint256",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [
-      {
-        internalType: "address",
-        name: "_token",
-        type: "address",
-      },
-    ],
+    inputs: [{ internalType: "address", name: "_token", type: "address" }],
     name: "mockOwnerOptimismBridgeAddress",
     outputs: [],
     stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    inputs: [],
-    name: "optimismBridgeInstance",
-    outputs: [
-      {
-        internalType: "contract MockOptimismBridge",
-        name: "",
-        type: "address",
-      },
-    ],
-    stateMutability: "view",
     type: "function",
   },
   {
@@ -228,34 +62,47 @@ export const goerliABI = [
     type: "function",
   },
   {
-    inputs: [
-      {
-        internalType: "bytes",
-        name: "",
-        type: "bytes",
-      },
-    ],
+    inputs: [{ internalType: "bytes", name: "", type: "bytes" }],
     name: "performUpkeep",
     outputs: [],
     stateMutability: "nonpayable",
     type: "function",
   },
+  { inputs: [], stateMutability: "nonpayable", type: "constructor" },
   {
-    inputs: [
-      {
-        internalType: "uint256",
-        name: "",
-        type: "uint256",
-      },
-    ],
-    name: "queue",
+    inputs: [],
+    name: "first",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "last",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "optimismBridgeInstance",
     outputs: [
-      {
-        internalType: "address",
-        name: "",
-        type: "address",
-      },
+      { internalType: "contractMockOptimismBridge", name: "", type: "address" },
     ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "Owner",
+    outputs: [{ internalType: "address", name: "", type: "address" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    name: "queue",
+    outputs: [{ internalType: "address", name: "", type: "address" }],
     stateMutability: "view",
     type: "function",
   },
